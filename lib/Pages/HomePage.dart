c7586ae66dc8c667edc5f130ac4af2c5315241ff
christianbernal7 <christiandave.bernal63@gmail.com>
2024-12-18 15:01:29 +0800

    Dashboard/Sidebar

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:capstone/Pages/Sign.dart';
 import 'package:capstone/components/jobsyncflow.dart';
 import 'package:capstone/components/popularcategories.dart';
-import 'package:capstone/pages/Applicant/findjob.dart';
-import 'package:capstone/pages/Applicant/findemployer.dart';
+
+import 'package:capstone/components/drawer.dart';
 
 class JobSyncApp extends StatelessWidget {
   @override
@@ -38,9 +38,9 @@ class JobSyncApp extends StatelessWidget {
                 Navigator.push(
                   context,
                   PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) => SignScreen(), // Your login screen class
-                    transitionDuration: Duration.zero, // Instant transition
-                    reverseTransitionDuration: Duration.zero, // Instant reverse transition
+                    pageBuilder: (context, animation, secondaryAnimation) => SignScreen(),
+                    transitionDuration: Duration.zero, 
+                    reverseTransitionDuration: Duration.zero, 
                   ),
                 );
               },
@@ -55,115 +55,50 @@ class JobSyncApp extends StatelessWidget {
             ),
           ],
         ),
-        // Drawer widget for menu options
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue, // Set the background color to blue
-                ),
-                accountName: Text('Jobsync User'),
-                accountEmail: Text('user@example.com'),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.account_circle, size: 50, color: Colors.blue),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
-                onTap: () {
-                  // TODO: Implement navigation to Home
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.search),
-                title: Text('Find Job'),
-                onTap: () {
-                  Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => FindJob()),
-                );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.business),
-                title: Text('Employers'),
-                onTap: () {
-                  Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FindEmployer()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.dashboard),
-                title: Text('Dashboard'),
-                onTap: () {
-                  // TODO: Implement navigation to Dashboard screen
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.notifications),
-                title: Text('Job Alerts'),
-                onTap: () {
-                  // TODO: Implement navigation to Job Alerts screen
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.support),
-                title: Text('Customer Support'),
-                onTap: () {
-                  // TODO: Implement navigation to Customer Support screen
-                },
-              ),
-            ],
-          ),
-        ),
+        // Use the CustomDrawer widget here
+        drawer: CustomDrawer(),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-  padding: const EdgeInsets.all(16.0),
-  child: Row(
-    children: [
-      Container(
-        height: 55,
-        width: 75,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20), // Existing border radius
-          border: Border.all(
-            color: Colors.grey, // Border color
-            width: 1, // Border width
-          ),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: Row(
-          children: [
-            Image.asset('assets/flags/ph.png', width: 24),
-            SizedBox(width: 8),
-            Text('PH'),
-          ],
-        ),
-      ),
-      SizedBox(width: 16),
-      Expanded(
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Job title, keyword, company',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            prefixIcon: Icon(Icons.search),
-          ),
-        ),
-      ),
-    ],
-  ),
-),
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 55,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20), 
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/flags/ph.png', width: 24),
+                          SizedBox(width: 8),
+                          Text('PH'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Job title, keyword, company',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          prefixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               SizedBox(height: 16),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -181,7 +116,7 @@ class JobSyncApp extends StatelessWidget {
                       'Suggestion: Designer, Programming, Digital Marketing, Video, Animation',
                       textAlign: TextAlign.center,
                       style: TextStyle(color: Colors.black),
-                    ), // Add your illustration image
+                    ),
                     SizedBox(height: 16),
                     Wrap(
                       spacing: 16,
@@ -200,7 +135,6 @@ class JobSyncApp extends StatelessWidget {
                   ],
                 ),
               ),
-             
             ],
           ),
         ),
@@ -209,7 +143,6 @@ class JobSyncApp extends StatelessWidget {
     );
   }
 
-  // Corrected and unified buildStatCard function
   Widget buildStatCard(String iconPath, String count, String label) {
     return Container(
       width: 150,
@@ -223,13 +156,13 @@ class JobSyncApp extends StatelessWidget {
         children: [
           Image.asset(
             iconPath,
-            height: 36, // Adjust the size of the icon
+            height: 36, 
             width: 36,
             fit: BoxFit.contain,
           ),
           SizedBox(height: 8),
           Text(
-            count, // Use the count parameter here
+            count, 
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 4),
